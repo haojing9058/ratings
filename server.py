5394c2da83aa32dfb0e0affef3ca331e163cc900
Hackbright Student <no-reply@hackbrightacademy.com>
2018-02-01 11:59:26 -0800

    finished user details

--- server.py
+++ server.py
@@ -39,7 +39,7 @@ def user_list():
 def register_form():
     """A form ask for email address and password
     and a form that shows the registration form"""
-       
+
     return render_template("registration_form.html")
 
 
@@ -52,14 +52,13 @@ def register_process():
     email = request.form.get("email")
     password = request.form.get("password")
 
-    #get all emails in db
-    email_ls = db.session.query(User.email).all()
+    check_email = User.query.filter_by(email=email).first()
 
-    if email in email_ls:
-        #get the password of this user and compare
-        if password == User.email.password:
+    if check_email:
+        check_password = User.query.filter(User.email==email, User.password==password).first()
+        if check_password:
             flash("You're logged in!")
-            return redirect("/")
+            return redirect("/user/user_id")
         else:
             flash("wrong password!")
             return redirect("/")
@@ -70,6 +69,35 @@ def register_process():
         db.session.commit()
 
     return redirect("/")
+
+
+@app.route("/logout")
+def log_out():
+    """Log out the user"""
+
+    db.session.remove()
+    flash("Logged out")
+    return redirect("/")
+
+
+@app.route("/users/<user_id>")
+def display_user(user_id):
+    """Display informationa about a particular user"""
+
+    user_details = User.query.filter(User.user_id==user_id).first()
+
+    user_ratings = Rating.query.filter(Rating.user_id==user_id).all()
+
+    user_movies = []
+    for r in user_ratings:
+        movie = Movie.query.get(r.movie_id).title
+        score = r.score
+        user_movies.append((movie, score))
+
+    return render_template("user_info.html",
+                           user_details=user_details,
+                           user_movies=user_movies)
+
 
 if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the
